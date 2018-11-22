9b9433a8a74fada60b74d4899a1576e26b3f1081
Ernad Husremovic <hernad@bring.out.ba>
2018-11-22 13:30:17 +0100

    git and hypert teme

---
+++ extensions/git/src/typings/refs.d.ts
@@ -0,0 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+/// <reference path='../../../../src/hypert.d.ts'/>
+/// <reference path='../../../../src/hypert.proposed.d.ts'/>
