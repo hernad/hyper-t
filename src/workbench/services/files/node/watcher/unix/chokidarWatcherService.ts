bd46e90e323027f4d08b1579dfd22e7e5ef5ed41
Ernad Husremovic <hernad@bring.out.ba>
2018-11-22 13:29:28 +0100

    package.json engines.hypert - extensions/theme i git rade

--- src/workbench/services/files/node/watcher/unix/chokidarWatcherService.ts
+++ src/workbench/services/files/node/watcher/unix/chokidarWatcherService.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as chokidar from 'hypert-chokidar';
+import * as chokidar from 'vscode-chokidar';
 import * as fs from 'fs';
 
 import * as gracefulFs from 'graceful-fs';
